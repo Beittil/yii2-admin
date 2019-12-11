bf30b57a38632ec83dc276f01f52f01ddd4619c8
Beittil <dennis.stolwijk@conligo.nl>
2019-12-11 15:01:06 +0100

    Update assignment

--- views/default/assignment/index.php
+++ views/default/assignment/index.php
@@ -31,11 +31,11 @@ use dee\angular\NgView;
                     <tr ng-repeat="model in rows">
                         <td width="35px;">{{(provider.page - 1) * provider.itemPerPage + $index + 1}}</td>
                         <td>{{model.username}}</td>
-                        <td><a ng-repeat="role in model.assignments" ng-href="#{{role.type == 1?'role':'permission'}}/{{role.name}}">
+                        <td><a ng-repeat="role in model.assignments" ng-href="#!/{{role.type == 1?'role':'permission'}}/{{role.name}}">
                                 <span class="label" ng-class="{'label-danger':role.type == 1,'label-success':role.type == 2}">{{role.name}}</span></a>
                         </td>
                         <td width="40px">
-                            <a ng-href="#/assignment/{{model.id}}"><span class="glyphicon glyphicon-eye-open"></span></a>
+                            <a ng-href="admin#!/assignment/{{model.id}}"><span class="glyphicon glyphicon-eye-open"></span></a>
                         </td>
                     </tr>
                 </tbody>
